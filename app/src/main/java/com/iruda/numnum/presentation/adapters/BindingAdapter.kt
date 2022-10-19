ca5eb0bba73ef5cb8158e15548bcf8945ade7dc7
NjordJ <nj0rd3841@gmail.com>
2022-10-19 21:08:34 +0500

    Data binding with ViewModel

--- app/src/main/java/com/iruda/numnum/presentation/adapters/BindingAdapter.kt
+++ app/src/main/java/com/iruda/numnum/presentation/adapters/BindingAdapter.kt
@@ -1,10 +1,21 @@
 package com.iruda.numnum.presentation.adapters
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.iruda.numnum.R
 import com.iruda.numnum.domain.entities.GameResult
+
+interface OnOptionClickListener {
+
+    fun onOptionClick(option: Int)
+}
+
+// GameFinishedFragment
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -58,3 +69,43 @@ private fun getImageResId(isWin: Boolean): Int {
         R.drawable.ic_sad
     }
 }
+
+// GameFragment
+
+@BindingAdapter("answersProgress")
+fun bindAnswerProgress(progressBar: ProgressBar, progress: Int) {
+    progressBar.setProgress(progress, true)
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
+
